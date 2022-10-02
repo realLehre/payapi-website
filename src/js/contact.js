c26b482f754e9d20618d94eb7bef41e60ae3fdc1
David Egbuwalo <lereegbuwalo@gmail.com>
2022-10-02 21:47:19 +0100

    update

---
+++ src/js/contact.js
@@ -0,0 +1,94 @@
+const form = document.querySelector('#contact-form');
+const inputs = document.querySelectorAll('.form-input');
+const labels = document.querySelectorAll('.form-label');
+const textArea = document.getElementById('message');
+
+let textAreaValue = '';
+
+const textErrorMessage = textArea.nextElementSibling;
+textArea.addEventListener('keyup', (e) => {
+  textAreaValue = textArea.value;
+  if (textArea.value !== '') {
+    e.target.previousElementSibling.classList.add('hide');
+    textArea.style.borderBottom = '3px solid #6c8294';
+    textErrorMessage.classList.remove('showError');
+  } else {
+    e.target.previousElementSibling.classList.remove('hide');
+    e.target.previousElementSibling.style.color = '#ff0000';
+    textArea.style.borderBottom = '1px solid #ff0000';
+    textErrorMessage.classList.add('showError');
+  }
+});
+
+inputs.forEach((input, index) => {
+  const errorMessage = input.nextElementSibling;
+  input.addEventListener('keyup', (e) => {
+    inputValue = input.value;
+    if (input.value !== '') {
+      labels[index].classList.add('hide');
+      input.style.borderBottom = '2px solid #6c8294';
+      errorMessage.classList.remove('showError');
+    } else {
+      labels[index].classList.remove('hide');
+      labels[index].style.color = '#ff0000';
+      input.style.borderBottom = '1px solid #ff0000';
+      errorMessage.classList.add('showError');
+    }
+  });
+});
+
+let check = false;
+function validateEmail() {
+  const email = inputs[1];
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!re.test(email.value)) {
+    document.querySelector('.email-format-error').classList.add('showError');
+    email.style.borderBottom = '1px solid #ff0000';
+    try {
+      document.querySelector('.email-error').classList.remove('showError');
+    } catch {}
+  } else {
+    document.querySelector('.email-format-error').classList.remove('showError');
+    email.style.borderBottom = '2px solid #6c8294';
+    check = true;
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  inputs.forEach((input, index) => {
+    const errorMessage = input.nextElementSibling;
+    if (input.value == '') {
+      errorMessage.classList.add('showError');
+    } else {
+      errorMessage.classList.remove('showError');
+      validateEmail();
+    }
+  });
+
+  if (textAreaValue == '') {
+    textErrorMessage.classList.add('showError');
+  } else {
+    textErrorMessage.classList.remove('showError');
+  }
+
+  try {
+    if (
+      check &&
+      inputs[0].value != '' &&
+      inputs[1].value != '' &&
+      inputs[2].value != '' &&
+      textAreaValue != ''
+    ) {
+      inputs.forEach((input) => {
+        input.value = '';
+        input.style.borderBottom = '1px solid #6c8294';
+      });
+
+      textArea.value = '';
+      textArea.style.borderBottom = '1px solid #6c8294';
+    }
+  } catch (err) {}
+
+  e.preventDefault();
+});
